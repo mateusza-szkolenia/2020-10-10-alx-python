47cf4b64f590379d680e3f5ef7177d54ea9c6d3e
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-11 17:08:59 +0200

    zadanie 1

---
+++ dzien02/zadanie01.py
@@ -0,0 +1,20 @@
+li = [ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
+
+# drugi element
+print( li[2] )
+#lub w zależności od interpretacji:
+print( li[1] )
+
+#I przedostatni element
+print( li[-2] )
+
+# I elementy od trzeciego do siódmego (włacznie)
+print( li[ 3:7+1 ] )
+# lub w zależności od interpretacji:
+print( li[ 2:7 ] )
+
+# I co trzeci element
+print( li[::3] )
+
+#I co drugi element liczac od konca
+print( li[ ::-2 ] )
