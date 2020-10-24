266e806ee2f6cdcb25aa26c87f8a29f2c95f94d0
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 14:01:06 +0200

    problem: szukanie najmniejszej

---
+++ dzien03/zadanie06-pre1.py
@@ -0,0 +1,21 @@
+# Mając daną listę liczb, znaleźć najmniejszą z nich
+
+liczby = [ 1, 3, 100, 50, 20, 17, 60, 70, 80, 111, 50, 13 ]
+
+# POPRAWIC:
+
+najwieksza = 0
+
+for biezaca in liczby:
+    if biezaca > najwieksza:
+        najwieksza = biezaca
+
+# nie chodzi o uzycie max(), tylko o petle for
+
+print( f"Najmniejsza liczba to: {najwieksza}" )
+
+
+
+
+# znaleźć najdłuższe słowo:
+# slowa = [ 'kot', 'zolw', 'zyrafa', 'pies', 'jaskolka' ]
