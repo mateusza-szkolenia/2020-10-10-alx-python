2f3b56ba8be253654739e6b9429039580dfb19ab
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 16:25:28 +0200

    zadanie 3 podatki - rozwiazanie drugi

--- dzien03/petla-for-08.py
+++ dzien03/petla-for-08.py
@@ -1,6 +1,7 @@
 zarobki = [ 4000, 6000, 3000, 9000, 20000, 7500 ]
 
 # wprowadzamy stawki:
+# 30% dla zarabiajacych >= 6500
 # 15% dla zarabiajacych >= 4500
 # 5% dla pozostalych
 
@@ -9,6 +10,6 @@ podatek = 0
 # POPRAWIC
 
 for z in zarobki:
-    podatek += z * ( 0.15 if z >= 4500 else 0.05 )
+    podatek += z * ( 0.3 if z >= 6500 else ( 0.15 if z >= 4500 else 0.05 ) )
 
 print(f"Calkowity zebrany podatek: {podatek}")
