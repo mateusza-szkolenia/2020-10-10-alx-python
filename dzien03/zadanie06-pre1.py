2b70fad5781ac8a8e71298331a7a09e029786033
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 14:13:41 +0200

    znajdowanie minimum - lepszy pomysl

--- dzien03/zadanie06-pre1.py
+++ dzien03/zadanie06-pre1.py
@@ -4,7 +4,8 @@ liczby = [ 3, 100, 50, 20, 17, 60, 2, 70, 80, 111, 50, 13 ]
 
 # POPRAWIC:
 
-najmniejsza = 1000
+#najmniejsza = 1000
+najmniejsza = liczby[0]
 
 for biezaca in liczby:
     if biezaca < najmniejsza:
