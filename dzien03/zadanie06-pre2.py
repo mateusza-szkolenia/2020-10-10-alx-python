a08345a4dec2f37aa671b2670d6714bc59fcc251
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 14:32:20 +0200

    szukanie min i max w jednym przejsciu petli

---
+++ dzien03/zadanie06-pre2.py
@@ -0,0 +1,19 @@
+# Mając daną listę liczb, znaleźć najmniejszą i największą z nich
+
+liczby = [ 3, 100, 50, 20, 17, 60, 2, 70, 80, 111, 50, 13 ]
+
+najmniejsza = None
+najwieksza = None
+
+for biezaca in liczby:
+    if najmniejsza == None:
+        najmniejsza = biezaca
+    if biezaca < najmniejsza:
+        najmniejsza = biezaca
+    if najwieksza == None:
+        najwieksza = biezaca
+    if biezaca > najwieksza:
+        najwieksza = biezaca
+
+print( f"Najmniejsza liczba to: {najmniejsza}" )
+print( f"Najwieksza liczba to: {najwieksza}" )
