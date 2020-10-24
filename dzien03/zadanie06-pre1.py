eafd68bae7e7a48b1f459246e2c89a6ac9deded4
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 14:12:25 +0200

    znajdowanie minimum - naiwny pomysl

--- dzien03/zadanie06-pre1.py
+++ dzien03/zadanie06-pre1.py
@@ -4,15 +4,14 @@ liczby = [ 3, 100, 50, 20, 17, 60, 2, 70, 80, 111, 50, 13 ]
 
 # POPRAWIC:
 
-najwieksza = 0
+najmniejsza = 1000
 
 for biezaca in liczby:
-    if biezaca > najwieksza:
-        najwieksza = biezaca
+    if biezaca < najmniejsza:
+        najmniejsza = biezaca
 
-# nie chodzi o uzycie max(), tylko o petle for
 
-print( f"Najmniejsza liczba to: {najwieksza}" )
+print( f"Najmniejsza liczba to: {najmniejsza}" )
 
 
 
