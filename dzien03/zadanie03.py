8537acb481f7cb22b417d58d2c826ce6b8ceb683
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 09:56:57 +0200

    rozwiązanie

--- dzien03/zadanie03.py
+++ dzien03/zadanie03.py
@@ -3,11 +3,11 @@ liczby = [45, -33, 0, 67, 100, 2000, -1 ]
 dod = 0
 uje = 0
 
-# jakiś kod
-#
-#
-#
-
+for n in liczby:
+    if n < 0:
+        uje += 1
+    elif n > 0:
+        dod += 1
 
 print( f"Dodatnie: {dod}")
 print( f"Ujemne: {uje} ")
