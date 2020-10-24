8e1b4c497cfc6da6bc848a2e243922380c993c6e
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 16:08:44 +0200

    zadanie: podatki

---
+++ dzien03/petla-for-08.py
@@ -0,0 +1,5 @@
+zarobki = [ 4000, 6000, 3000, 9000, 20000, 7500 ]
+
+# zalozmy ze jest podatek liniowy i chcemy policzyc podatek o wartosci 10% od kazdego z tych wynagrodzen
+# napisac sumaryczny podatek zaplacony przez te osoby
+
