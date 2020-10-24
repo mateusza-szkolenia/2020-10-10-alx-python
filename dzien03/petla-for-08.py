1c460ff5d43c82d811580f4b075f756165dbf61f
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 16:12:29 +0200

    zadanie: podatki - rozwiazanie 1

--- dzien03/petla-for-08.py
+++ dzien03/petla-for-08.py
@@ -1,5 +1,11 @@
 zarobki = [ 4000, 6000, 3000, 9000, 20000, 7500 ]
 
 # zalozmy ze jest podatek liniowy i chcemy policzyc podatek o wartosci 10% od kazdego z tych wynagrodzen
-# napisac sumaryczny podatek zaplacony przez te osoby
+# obliczyc sumaryczny podatek zaplacony przez te osoby
 
+podatek = 0
+
+for z in zarobki:
+    podatek += z * 0.1
+
+print(f"Calkowity zebrany podatek: {podatek}")
