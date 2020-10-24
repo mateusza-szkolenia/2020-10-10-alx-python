791fafb914feb9e0f9d539568be425426a8ad8b8
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 13:41:20 +0200

    zadanie

---
+++ dzien03/zadanie-06-pre0.py
@@ -0,0 +1,15 @@
+# Mając daną listę liczb, znaleźć największą z nich
+
+liczby = [ 1, 3, 100, 50, 20, 17, 60, 70, 80, 111, 50, 13 ]
+
+# ZAIMPLEMENTOWAC
+
+# nie chodzi o uzycie max(), tylko o petle for
+
+print( f"Największa liczba to: {111}" )
+
+
+
+
+# znaleźć najdłuższe słowo:
+# slowa = [ 'kot', 'zolw', 'zyrafa', 'pies', 'jaskolka' ]
