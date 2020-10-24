d4368ae58285e3c637631a6a34785a50b57d15fb
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 16:19:17 +0200

    zadanie 3 podatki - rozwiazanie pierwsze

--- dzien03/petla-for-08.py
+++ dzien03/petla-for-08.py
@@ -9,9 +9,9 @@ podatek = 0
 # POPRAWIC
 
 for z in zarobki:
-    if z <= 4200:
-        pass
-    else:
+    if z >= 4500:
         podatek += z * 0.15
+    else:
+        podatek += z * 0.05
 
 print(f"Calkowity zebrany podatek: {podatek}")
