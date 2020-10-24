9099671f254592152a37d2fbf6a0486b0a112131
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 13:59:09 +0200

    znajdowanie najwiekszej nieujemnej liczby

--- dzien03/zadanie06-pre0.py
+++ dzien03/zadanie06-pre0.py
@@ -4,9 +4,15 @@ liczby = [ 1, 3, 100, 50, 20, 17, 60, 70, 80, 111, 50, 13 ]
 
 # ZAIMPLEMENTOWAC
 
+najwieksza = 0
+
+for biezaca in liczby:
+    if biezaca > najwieksza:
+        najwieksza = biezaca
+
 # nie chodzi o uzycie max(), tylko o petle for
 
-print( f"Największa liczba to: {111}" )
+print( f"Największa liczba to: {najwieksza}" )
 
 
 
