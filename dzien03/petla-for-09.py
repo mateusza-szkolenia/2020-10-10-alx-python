b03fe46fa4d8a2e427e750834ab265e26d25d569
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 16:29:09 +0200

    petla for - c.d.

---
+++ dzien03/petla-for-09.py
@@ -0,0 +1,10 @@
+liczby = [ 1, 4, 9, 13, 27 ]
+
+# chcialbym policzyc 3 potegi tych liczb i zapisac je na osobnej liscie liczby3
+
+liczby3 = []
+
+for l in liczby:
+    liczby3.append( l ** 3 )
+
+print(liczby3)
