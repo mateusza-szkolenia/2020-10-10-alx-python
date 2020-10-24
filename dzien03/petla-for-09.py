72c4d342d2e40acf48e117a08495657e4e1336fb
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-24 16:47:26 +0200

    petla for

--- dzien03/petla-for-09.py
+++ dzien03/petla-for-09.py
@@ -1,10 +1,15 @@
 liczby = [ 1, 4, 9, 13, 27 ]
 
-# chcialbym policzyc 3 potegi tych liczb i zapisac je na osobnej liscie liczby3
+# chcialbym policzyc 3 potegi tych liczb i zapisac te,
+# ktorych wartosc modulo 10 jest <= 5
+# na osobnej liscie liczby3
+#
 
 liczby3 = []
 
 for l in liczby:
-    liczby3.append( l ** 3 )
+    if ( l ** 3 ) % 10 <= 5:
+        liczby3.append( l )
 
 print(liczby3)
+
