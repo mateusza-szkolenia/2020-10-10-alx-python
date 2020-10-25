36431d24cb8445bc7e93f6b490aac3d110d8edf8
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 14:38:09 +0100

    zadanie 11 - pierwsza czesc rozwiazania

--- dzien04/zadanie11.py
+++ dzien04/zadanie11.py
@@ -3,3 +3,15 @@
 # parzystymi w zakresie 0-100 - w tym celu skorzystaj z operatora
 # iloczynu.
 
+liczby = [ 10, 20, 10, 30, 13, 13, 30, 10, 20, 11, 10, 777, 800 ]
+
+print(f"Liczby: {liczby}")
+print(f"Liczba liczb: {len(liczby)} ")
+
+bez_powt = set( liczby )
+print(f"Liczby unikalne: {bez_powt}")
+
+bez_powt = list(set( liczby ))
+print(f"Liczby unikalne: {bez_powt}")
+
+
