753861ca9b700060f5fac18c8dab793d7ac95a99
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 10:43:05 +0100

    wyrazenie listowe - jednolijkowe

--- dzien04/zadanie07.py
+++ dzien04/zadanie07.py
@@ -2,10 +2,9 @@ napis = "ala ma kota, ola ma psa, robert ma żółwia, a ula ma chomika"
 
 samogloski = "aeiouyó"
 
-ile_samoglosek = 0
-
-for litera in napis:
-    if litera in samogloski:
-        ile_samoglosek += 1
+# ile_samoglosek = [ litera for litera in napis ]
+# ile_samoglosek = [ litera for litera in napis if litera in samogloski ]
+# ile_samoglosek = len( [ litera for litera in napis if litera in samogloski ] )
+ile_samoglosek = len( [ None for litera in napis if litera in samogloski ] )
 
 print(f"Samoglosek: {ile_samoglosek}")
