d694d098f06313c53f9882c0b3a4ad7b1d883ea5
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 10:16:35 +0100

    sztuczki z wyrazeniami listowymi i slownikami

--- dzien04/wyrazenia-listowe-02.py
+++ dzien04/wyrazenia-listowe-02.py
@@ -5,32 +5,71 @@ pracownicy = [
     {
         "imie":"Mateusz",
         "nazwisko":"Adamowski",
-        "stanowisko":"trener"
+        "stanowisko":"trener",
+        "szkolenia":["Linux","Python","C","C++","IPv6"]
     },
     {
         "imie":"Jacek",
         "nazwisko":"Fiok",
-        "stanowisko":"szef"
+        "stanowisko":"szef",
+        "wzrost":1.80
     },
     {
         "imie":"Magda",
         "nazwisko":"W.",
-        "stanowisko":"ksiegowosc"
+        "stanowisko":"ksiegowosc",
+        "ulubiony_kolor":["czarny"]
     },
     {
         "imie":"Agata",
         "nazwisko":"P.",
-        "stanowisko":"trener"
+        "stanowisko":"trener",
+        "szkolenia":['Python','Excel','SQL',"Access"]
     },
     {
         "imie":"Tadeusz",
         "nazwisko":"X.",
-        "stanowisko":"trener"
+        "stanowisko":"trener",
+        "szkolenia":["Excel","Access","SQL","Oracle"]
     },
     {
         "imie":"Kuba",
-        "stanowisko":"admin"
+        "stanowisko":"admin",
+        "zwolniony":2013
+    },
+    {
+        "imie":"Kuba",
+        "stanowisko":"trener",
+        "nazwisko":"XXX"
     }
-
 ]
 
+for pracownik in pracownicy:
+    print( f"Pracownik: {pracownik['imie']} { pracownik['nazwisko'] if 'nazwisko' in pracownik else '***' }" )
+
+# napisac ilu jest trenerow
+
+ilu_trenerow = 0
+for pracownik in pracownicy:
+    if pracownik['stanowisko'] == 'trener':
+        ilu_trenerow += 1
+
+print(f"Trenerow: {ilu_trenerow}")
+
+ilu_trenerow2 = len( [ 1 for p in pracownicy if p['stanowisko'] == 'trener' ] )
+print(f"Trenerow: {ilu_trenerow2} ")
+
+# napisac nazwiska trenerow mezczyzn
+imiona_tren_mez = "\n".join([
+    f"{p['imie']} {p['nazwisko']}"
+    for p in pracownicy
+    if
+        p['stanowisko'] == 'trener' and (
+            p['imie'] in imiona_meskie_a or (
+               p['imie'][-1] != 'a'
+               and p['imie'] not in imiona_zenskie_bez_a
+            )
+        )
+])
+
+print( f"Trenerzy mezczyzni:\n{imiona_tren_mez}" )
