5d1c16b18648b40980bf660ce2f45bcffc01ff39
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 15:46:22 +0100

    zwracanie wartosci z funkcji (wyniku)

--- dzien04/funkcje-01.py
+++ dzien04/funkcje-01.py
@@ -5,7 +5,18 @@ def przelicz_temp_C( tC ):
     tF = 1.8 * tC + 32
     print(f"{tC}°C = {tF}°F")
 
+def przelicz_C_na_F( tC ):
+    tF = 1.8 * tC + 32
+    return tF
+
 pomiary = [ 12, 13, 15, 13, 10, 9, 6, 10, -1, -2, 4 ]
 
+# zimno < 60°F
+# cieplo >= 60°F
+
+# wyswietlic tylko te pomiary kiedy jest cieplo
+
 for tempC in pomiary:
-    przelicz_temp_C( tempC )
+    tempF = przelicz_C_na_F( tempC )
+    print(f"{tempC}°C ===> {tempF}°F")
+
