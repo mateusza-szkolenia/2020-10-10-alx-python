d63c8c0d8b8af3df0520190ac945d5bf992ab5d0
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 14:44:21 +0100

    zadanie 11 - wpisywanie liczb przez uzytkownika

--- dzien04/zadanie11.py
+++ dzien04/zadanie11.py
@@ -3,7 +3,14 @@
 # parzystymi w zakresie 0-100 - w tym celu skorzystaj z operatora
 # iloczynu.
 
-liczby = [ 10, 20, 10, 30, 13, 13, 30, 10, 20, 11, 10, 777, 800 ]
+#liczby = [ 10, 20, 10, 30, 13, 13, 30, 10, 20, 11, 10, 777, 800 ]
+liczby = []
+
+while True:
+    x = input( "Podaj liczbe lub napisz KONIEC: " )
+    if x == "KONIEC":
+        break
+    liczby.append( int(x) )
 
 print(f"Liczby: {liczby}")
 print(f"Liczba liczb: {len(liczby)} ")
