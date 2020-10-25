427343358e271d5cfffdbf0c3306c11a7177b2d8
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 14:20:03 +0100

    zadanie 11

---
+++ dzien04/zadanie11.py
@@ -0,0 +1,5 @@
+# Napisz program zliczajacy liczbe unikalnych liczb wprowadzonych
+# przez uzytkownika. Sprawdz jak duzo z tych liczb jest liczbami
+# parzystymi w zakresie 0-100 - w tym celu skorzystaj z operatora
+# iloczynu.
+
