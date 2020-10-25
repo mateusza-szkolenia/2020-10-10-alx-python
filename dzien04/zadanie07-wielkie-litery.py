8088e8c904ea6fcc644bcbdcc93f39bfe562c452
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 10:46:38 +0100

    wielkie litery

---
+++ dzien04/zadanie07-wielkie-litery.py
@@ -0,0 +1,10 @@
+napis = "Ala ma kota, Ola ma psa, Robert ma żółwia, a Ula ma chomika"
+
+samogloski = "aeiouyó"
+
+# ile_samoglosek = [ litera for litera in napis ]
+# ile_samoglosek = [ litera for litera in napis if litera in samogloski ]
+# ile_samoglosek = len( [ litera for litera in napis if litera in samogloski ] )
+ile_samoglosek = len( [ None for litera in napis if litera.lower() in samogloski ] )
+
+print(f"Samoglosek: {ile_samoglosek}")
