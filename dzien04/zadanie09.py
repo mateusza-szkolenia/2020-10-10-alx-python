bb082ae9089b1d731ea9e200f9801096d15abe9e
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 14:00:30 +0100

    zatrzymanie programu przy braku produktu

--- dzien04/zadanie09.py
+++ dzien04/zadanie09.py
@@ -1,10 +1,13 @@
+import sys
+
 oferta = [
     { "nazwa" : "ziemniaki", "cena" : 2.0 },
     { "nazwa" : "migdaly", "cena" : 40.00 },
     { "nazwa" : "maka", "cena" : 4.0 },
     { "nazwa" : "ryz", "cena" : 3.5 },
     { "nazwa" : "woda", "cena" : 1.0 },
-    { "nazwa" : "cytryny", "cena" : 8.0 }
+    { "nazwa" : "cytryny", "cena" : 8.0 },
+    { "nazwa" : "wodka", "cena" : 30.0 }
 ]
 
 # TUTAJ MA SIE WYPISAC OFERTA SKLEPU
@@ -15,6 +18,12 @@ print()
 
 # pytamy "Co chcesz kupic? "
 wybor = input("Co chcesz kupic? ")
+
+# sprawdzenie, czy w ogole taki produkt jest w sklepie
+
+if len( [ None for p in oferta if p['nazwa'] == wybor ] ) == 0:
+    print("Brak produktu w sklepie")
+    sys.exit()
 
 # pytamy "Ile kg?"
 waga = float( input("Ile kg? ") )
