165a5ffe8b0ea6fb24677add9a883b03e182a8cf
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 11:12:13 +0100

    pomijanie niewystepujacych

--- dzien04/zadanie07-bis.py
+++ dzien04/zadanie07-bis.py
@@ -1,11 +1,12 @@
 napis = "ala ma kota, ola ma psa, robert ma żółwia, a ula ma chomika"
 
-samogloski = "aeiouyó"
+samogloski = "aeiouyóąę"
 
 for s in samogloski:
     ile_razy = len( [ 1 for l in napis if l == s ] )
+    if ile_razy == 0:
+        continue
     print(f"{s} = {ile_razy}")
-
 
 # policz wystapienia kazdej samogloski oddzielnie, czyli np:
 # a = 12
