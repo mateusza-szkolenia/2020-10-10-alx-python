cd3de1ee55f2c37314e4a3a8d0446d89f22e6b2e
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 15:27:57 +0100

    pierwsza funkcja

---
+++ dzien04/funkcje-01.py
@@ -0,0 +1,18 @@
+def przelicz_temp_C( tC ):
+    print("Dzien dobry")
+    tF = 1.8 * tC + 32
+    print(f"{tC}C = {tF}F")
+    print("KONIEC FUNKCJI")
+
+pomiary = [ 12, 13, 15, 13, 10, 9, 6, 10, -1, -2, 4 ]
+
+for tempC in pomiary:
+    tempF = 1.8 * tempC + 32
+    print(f"{tempC}°C = {tempF}°F")
+
+przelicz_temp_C( 10 )
+
+pomiary = [ 10, 23, 15 ]
+for tempC in pomiary:
+    tempF = 1.8 * tempC + 32
+    print(f"{tempC}°C = {tempF}°F")
