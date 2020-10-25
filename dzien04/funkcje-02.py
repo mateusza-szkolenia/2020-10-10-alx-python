b246cd8d2c01ec0495a9a63e46963ab7298a724c
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 16:50:12 +0100

    kwadrat

--- dzien04/funkcje-02.py
+++ dzien04/funkcje-02.py
@@ -11,6 +11,9 @@ def obw_prostokata( a, b ):
 #   return 2 * a + 2 * b
 #   return 2 * ( a + b )
 
+def pole_kwadratu( bok ):
+    return pole_prostokata( bok, bok )
+
 prostokaty = [
     ( 100, 20 ),
     ( 300, 30 ),
@@ -21,3 +24,6 @@ for p in prostokaty:
     pole = pole_prostokata( p[0], p[1] )
     obw = obw_prostokata( p[0], p[1] )
     print(f"Pole prostokata o wymiarach: {p} wynosi {pole} a obw {obw}")
+
+for bbb in [ 12, 13 ]:
+    print(f"Pole kwadratu o boku {bbb} wynosi {pole_kwadratu(bbb)}")
