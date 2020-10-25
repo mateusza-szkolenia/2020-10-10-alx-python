017db804f31f3230af7333e4acbc3acf8a817c04
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 10:51:24 +0100

    zadanie zmodyfikowane

---
+++ dzien04/zadanie07-bis.py
@@ -0,0 +1,9 @@
+napis = "ala ma kota, ola ma psa, robert ma żółwia, a ula ma chomika"
+
+samogloski = "aeiouyó"
+
+# policz wystapienia kazdej samogloski oddzielnie, czyli np:
+# a = 12
+# i = 2
+# etc
+
