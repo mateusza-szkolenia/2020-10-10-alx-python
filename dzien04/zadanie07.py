53b020c61274620f4d7e2dccdc466962d3595621
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 10:33:12 +0100

    lepsze rozwiazanie

--- dzien04/zadanie07.py
+++ dzien04/zadanie07.py
@@ -1,14 +1,11 @@
-napis = "ala ma kota, ola ma psa, a ula ma chomika"
+napis = "ala ma kota, ola ma psa, robert ma żółwia, a ula ma chomika"
+
+samogloski = [ "a", "e", "o", "i", "u", "y", "ó" ]
 
 ile_samoglosek = 0
 
 for litera in napis:
-    if litera == "a":
+    if litera in samogloski:
         ile_samoglosek += 1
-    elif litera == "e":
-        ile_samoglosek += 1
-    elif litera == "i":
-        ile_samoglosek += 1
-# i tak dalej
 
 print(f"Samoglosek: {ile_samoglosek}")
