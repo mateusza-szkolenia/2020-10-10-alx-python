b222e10374ceab02562441af0471ecb7a5112cb5
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 12:10:28 +0100

    wiecej produktow w sklepiku

--- dzien04/zadanie09.py
+++ dzien04/zadanie09.py
@@ -1,6 +1,10 @@
 oferta = [
     { "nazwa" : "ziemniaki", "cena" : 2.0 },
-    { "nazwa" : "migdaly", "cena" : 40.00 }
+    { "nazwa" : "migdaly", "cena" : 40.00 },
+    { "nazwa" : "maka", "cena" : 4.0 },
+    { "nazwa" : "ryz", "cena" : 3.5 },
+    { "nazwa" : "woda", "cena" : 1.0 },
+    { "nazwa" : "cytryny", "cena" : 8.0 }
 ]
 
 # TUTAJ MA SIE WYPISAC OFERTA SKLEPU
