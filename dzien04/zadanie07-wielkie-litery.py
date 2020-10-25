0dd332c3c7a0afaedeafecc9a51159cee601511a
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 11:08:44 +0100

    komentarz

--- dzien04/zadanie07-wielkie-litery.py
+++ dzien04/zadanie07-wielkie-litery.py
@@ -2,6 +2,8 @@ napis = "Ala ma kota, Ola ma psa, Robert ma żółwia, a Ula ma chomika"
 
 samogloski = "aeiouyó"
 
+# policz wystapienia
+
 # ile_samoglosek = [ litera for litera in napis ]
 # ile_samoglosek = [ litera for litera in napis if litera in samogloski ]
 # ile_samoglosek = len( [ litera for litera in napis if litera in samogloski ] )
