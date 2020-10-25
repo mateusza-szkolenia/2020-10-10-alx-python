c66c15d536a3884df0ad4c80aeb4bcb43d1b42a7
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 09:30:50 +0100

    pracownicy

---
+++ dzien04/wyrazenia-listowe-02.py
@@ -0,0 +1,36 @@
+imiona_meskie_a = [ "Kuba", "Barnaba" ]
+imiona_zenskie_bez_a = [ "Viki", "Margaret" ]
+
+pracownicy = [
+    {
+        "imie":"Mateusz",
+        "nazwisko":"Adamowski",
+        "stanowisko":"trener"
+    },
+    {
+        "imie":"Jacek",
+        "nazwisko":"Fiok",
+        "stanowisko":"szef"
+    },
+    {
+        "imie":"Magda",
+        "nazwisko":"W.",
+        "stanowisko":"ksiegowosc"
+    },
+    {
+        "imie":"Agata",
+        "nazwisko":"P.",
+        "stanowisko":"trener"
+    },
+    {
+        "imie":"Tadeusz",
+        "nazwisko":"X.",
+        "stanowisko":"trener"
+    },
+    {
+        "imie":"Kuba",
+        "stanowisko":"admin"
+    }
+
+]
+
