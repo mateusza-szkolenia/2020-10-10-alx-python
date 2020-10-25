f5a8aa31eb8f33b34f5b423958ce9210c6644691
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 11:09:00 +0100

    pierwsze rozwiazanie zliczajace poszczegolne samogloski

--- dzien04/zadanie07-bis.py
+++ dzien04/zadanie07-bis.py
@@ -2,6 +2,14 @@ napis = "ala ma kota, ola ma psa, robert ma żółwia, a ula ma chomika"
 
 samogloski = "aeiouyó"
 
+for s in samogloski:
+    ile_razy = 0
+    for l in napis:
+        if l == s:
+            ile_razy += 1
+    print(f"{s} = {ile_razy}")
+
+
 # policz wystapienia kazdej samogloski oddzielnie, czyli np:
 # a = 12
 # i = 2
