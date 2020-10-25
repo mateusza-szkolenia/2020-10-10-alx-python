06fc478901d28dfe3c11efddb0475c345614dd16
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 11:57:45 +0100

    operacje wyszukiwania w stringu

--- dzien04/lancuchy.py
+++ dzien04/lancuchy.py
@@ -2,3 +2,6 @@ napis = "ala ma kota a kot ma pchly"
 
 for x in napis:
     print( f"[ {x} ]", end=" " )
+
+print( napis.find("k") ) # podaje pozycje litery k (pierwszej!)
+print( napis.find("kot") ) # podaje pozycje slowa "kot" (pierwszego!)
