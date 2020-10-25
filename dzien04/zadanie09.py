0202d46f7a421b6ddce492eb18eab4bbeb2732ab
Mateusz Adamowski <m.adamowski@alx.pl>
2020-10-25 12:01:30 +0100

    szkic zadania 9

---
+++ dzien04/zadanie09.py
@@ -0,0 +1,11 @@
+oferta = [
+    { "produkt" : "ziemniaki", "cena" : 2.0 },
+    { "produkt" : "migdaly", "cena" : 40.00 }
+]
+
+# TUTAJ MA SIE WYPISAC OFERTA SKLEPU
+
+# pytamy "Co chcesz kupic? "
+# pytamy "Ile kg?"
+
+# podajemy cene
