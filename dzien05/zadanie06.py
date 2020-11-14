d4a9ed56a00298e0f9c79a1eb47998dff89cbf1a
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-14 16:02:36 +0100

    rekurencyjne splaszczanie listy

--- dzien05/zadanie06.py
+++ dzien05/zadanie06.py
@@ -3,3 +3,16 @@
 # Przykład uzycia:
 # >>> splaszcz([1, 2, 3, [4, 5, [6]], 7])
 # [1, 2, 3, 4, 5, 6, 7]
+
+xxx = [ 45, 67, [ "sss", 12, [ "x", 34], False, 11 ], 8, [ 23, 34 ] ]
+
+def splaszcz(lista):
+    wynik = []
+    for element in lista:
+        if type(element) != list:
+            wynik += [ element ]
+        elif type(element) == list:
+            wynik += splaszcz(element)
+    return wynik
+
+print(splaszcz(xxx))
