83fd4e6a2a46193a61d3a2eaac90220eb9266118
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-14 12:46:09 +0100

    zadanie 2

---
+++ dzien05/zadanie2.py
@@ -0,0 +1,5 @@
+#Napisz funkcje zwracajaca zbiór wszystkich znaków wystepujacych w
+#napisie wiecej niz zadana liczba razy.
+#Przykład uzycia:
+#>>> wiecej_niz('ala ma kota a kot ma ale', 3)
+#{'a', ' '}
