d1f5866aff1e22499e69aad300309b9626e970ce
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-14 13:02:11 +0100

    znajdowanie powtarzajacych sie liter

--- dzien05/zadanie2.py
+++ dzien05/zadanie2.py
@@ -3,3 +3,18 @@
 #Przykład uzycia:
 #>>> wiecej_niz('ala ma kota a kot ma ale', 3)
 #{'a', ' '}
+
+def wiecej_niz( napis, ile ):
+    litery = set( napis )
+    wynik = set()
+    for litera in litery:
+        ile2 = napis.count(litera)
+#        print( litera, ile2 )
+        if ile2 >= ile:
+            wynik.add( litera )
+    return wynik
+
+def wiecej_niz2( napis, ile ):
+    return { l for l in set( napis ) if napis.count(l) > ile }
+
+print( wiecej_niz2( "ala ma kota a kot ma psa", 3 ))
