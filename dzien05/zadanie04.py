fb1fb961c547d706afcde6664c3f1c340f65cff4
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-14 14:24:49 +0100

    zadanie 04 rozwiazanie

--- dzien05/zadanie04.py
+++ dzien05/zadanie04.py
@@ -8,12 +8,25 @@
 #
 # wynik: 'koszt 10 PLN\nkwota 10 brutto'
 
-formatuj(
+def formatuj( *args, **kwargs ):
+    wynik = ""
+    for napis in args:
+        #print(f"{napis}")
+        for klucz, wartosc in kwargs.items():
+            #print(f"-- {klucz} = {wartosc}")
+            napis = napis.replace( "$" + klucz, wartosc )
+            #print(f"Po zmianie: {napis}")
+        wynik = wynik + napis + "\n"
+    return wynik
+
+x = formatuj(
     "Nazywam sie $imie i pracuje w $firma",
     "$firma miesci sie w miescie $miasto",
     imie="Mateusz",
     miasto="Warszawa",
     firma="ALX"
 )
+
+print(x)
 
 # wynik ma byc: "Nazywam sie Mateusz i pracuje w ALX\nALX miesci sie w miescie Warszawa"
