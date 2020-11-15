c6d0e23a3685d35d35af6d6fc316753c6cf46ccd
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-15 11:45:49 +0100

    konwersja jednostek

--- dzien06/dlugosc.py
+++ dzien06/dlugosc.py
@@ -4,6 +4,8 @@ class Dlugosc:
         self._jednostka = jednostka
     def _wartosc_w_m(self):
         return self._wartosc * self._mnozniki[self._jednostka]
+    def _wartosc_w_jednostce(self, jednostka):
+        return self._wartosc_w_m() / self._mnozniki[jednostka]
     def __lt__(self, other):
         return self._wartosc_w_m() < other._wartosc_w_m()
     def __eq__(self, other):
@@ -28,8 +30,13 @@ class Dlugosc:
         return f"{self._wartosc} {self._jednostka}"
     def __repr__(self):
         return f"Dlugosc({repr(self._wartosc)}, {repr(self._jednostka)})"
+    def konwertuj_na(self, jednostka):
+        return Dlugosc( self._wartosc_w_jednostce(jednostka), jednostka )
     _mnozniki = { 'm': 1, 'km' : 1000, 'mi' : 1_609.344, 'cm' : 0.01, 'in' : 0.0254 }
     __rmul__ = __mul__
+
+#d = Dlugosc(1, 'mi')    # 1.609km => 160934 cm
+#print( d._wartosc_w_jednostce('cm') )
 
 d3 = Dlugosc( 50, 'm' )
 d4 = d3.konwertuj_na('in')
