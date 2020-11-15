c649d0e54e7d7cb9f032e357967a8ab471bda847
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-15 16:31:16 +0100

    zapis do pliku w trybie w

---
+++ dzien06/pliki2.py
@@ -0,0 +1,7 @@
+import random
+
+# otwiera plik do zapisu - KASUJAC CALA ZAWARTOSC
+f = open("wynik.txt", "w")
+
+# zapisuje tekst do pliku
+f.write("ala ma kota " + str(random.randint(1,100000) ) + "\n" )
