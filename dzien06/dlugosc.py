f2fb3dc3b69962f74d959fc6e3d16883840c00f6
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-15 10:01:53 +0100

    dodawanie Dlugosci

--- dzien06/dlugosc.py
+++ dzien06/dlugosc.py
@@ -8,11 +8,20 @@ class Dlugosc:
         return self._wartosc_w_m() < other._wartosc_w_m()
     def __eq__(self, other):
         return self._wartosc_w_m() == other._wartosc_w_m()
+    def __add__(self, other):
+        nowa_wartosc = self._wartosc_w_m() + other._wartosc_w_m()
+        nowa_jednostka = 'm'
+        return Dlugosc( nowa_wartosc, nowa_jednostka )
+    def __str__(self):
+        return f"{self._wartosc} {self._jednostka}"
+
     _mnozniki = { 'm': 1, 'km' : 1000, 'mi' : 1_609.344, 'cm' : 0.01, 'in' : 0.0254 }
 
-d1 = Dlugosc( 8000, 'm' )
-d2 = Dlugosc( 8, 'km' )
-d3 = Dlugosc( 2, 'mi' )
+d1 = Dlugosc( 2, 'mi' )
+d2 = Dlugosc( 3, 'km' )
 
-print( d1 == d2 )
+d3 = d1 + d2
+
+print( d3 )
+
 
