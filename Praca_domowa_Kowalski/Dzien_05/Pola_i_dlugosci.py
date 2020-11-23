32fb2ea224a900f53d7d453d7da198be0d51aa68
KrzKowalski <kkowalski1@protonmail.com>
2020-11-23 21:52:42 +0100

    upload

--- Praca_domowa_Kowalski/Dzien_05/Pola_i_dlugosci.py
+++ Praca_domowa_Kowalski/Dzien_05/Pola_i_dlugosci.py
@@ -42,10 +42,10 @@ class Dlugosc:
     def __mul__(self, other):
         if type( other ) in ( float, int ):
             return Dlugosc( self._wartosc * other, self._jednostka )
-        # jak zrobic "if type other == obiekt klasy Dlugosc?"
-        else:
+        # jak zrobic "if type other == obiekt klasy Dlugosc? Odpowiedz: isinstance(obiekt, klasa)"
+        elif isinstance(other, Dlugosc):
             wartosc_pola = self._wartosc_w_m() * other._wartosc_w_m()
-            return Pola(wartosc_pola, jednostka_kw='m2')
+            return Pole(wartosc_pola, jednostka_kw='m2')
         return None
     def __str__(self):
         return f"{self._wartosc:.1f} {self._jednostka}"
@@ -56,16 +56,19 @@ class Dlugosc:
     _mnozniki = { 'm': 1, 'km' : 1000, 'mi' : 1_609.344, 'cm' : 0.01, 'in' : 0.0254, 'mm' : 0.001, 'dm' : 0.1, 'ft' : 0.3048 }
     __rmul__ = __mul__
 
-
-class Pola:
+class Pole:
     def __init__(self, wartosc_pola, jednostka_kw='m2'):
         # assert wartosc_pola > 0 and jednostka_kw != '' and type(wartosc_pola) == int or float
         self._wartosc_pola = wartosc_pola
         self._jednostka_kw = jednostka_kw
     def __str__(self):
         return f"{self._wartosc_pola} {self._jednostka_kw}"
-d1 = Dlugosc("2 m")
-d2 = Dlugosc("4 m")
+
+d1 = Dlugosc("200 cm")
+d2 = Dlugosc("3 m")
 print(d1)
+print(d2)
 d3 = d1*d2
 print(d3)
+d4 = Pole("144 cm2")
+print(d4)
