8da1cad942a1d66c5e1b093455f7469e3d1d8747
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-28 15:23:12 +0100

    pierwsze pobieranie danych z NBP

--- dzien07/zadanie1.py
+++ dzien07/zadanie1.py
@@ -1,3 +1,9 @@
+import requests
+
+def pobierz_kurs( waluta, data ):
+    url = f"http://api.nbp.pl/api/exchangerates/rates/A/{waluta}/{data}/"
+    wynik = requests.get( url )
+    return wynik
 
 data = "2019-12-26"
 waluta = "EUR"
@@ -5,3 +11,4 @@ waluta = "EUR"
 # chcialbym miec funkcje, ktora pobierze kurs obowiazujacy danego dnia:
 # (czyli z tego dnia, lub jesli to swieto to z ostatniego roboczego)
 kurs = pobierz_kurs( waluta, data )
+print(kurs) # tutaj chcemy dostac liczbe float
