061e268a32136337b31afdd7df159d5fe1433221
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-28 15:55:47 +0100

    operacje walutowe

--- dzien07/zadanie1.py
+++ dzien07/zadanie1.py
@@ -9,9 +9,7 @@ def pobierz_kurs( waluta, data ):
     return requests.get( url ).json()['rates'][-1]['mid']
 
 portfel = {
-    "PLN" : 0,
-    "EUR" : 0,
-    "USD" : 0
+    "PLN" : 0
 }
 
 zlecenia = json.load( open("zlecenia.json") )
@@ -20,6 +18,8 @@ for z in zlecenia:
     dz = z['data']
     w = z['waluta']
     kurs = pobierz_kurs(w, dz)
+    if w not in portfel:
+        portfel[w] = 0
     if z['akcja'] == 'kup':
         portfel['PLN'] -= z['ile'] * kurs
         portfel[w] += z['ile']
@@ -27,5 +27,13 @@ for z in zlecenia:
         portfel['PLN'] += z['ile'] * kurs
         portfel[w] -= z['ile']
     print(z, kurs)
+
+dzisiaj = datetime.date.today().isoformat()
+for waluta, kwota in portfel.items():
+    if waluta == 'PLN':
+        continue
+    kurs = pobierz_kurs( waluta, dzisiaj )
+    portfel['PLN'] += kwota * kurs
+    portfel[waluta] = 0
 
 print(portfel)
