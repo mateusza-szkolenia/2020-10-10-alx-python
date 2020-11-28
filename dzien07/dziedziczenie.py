9962b0e299044f035998f03f9ef26202f4598de7
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-28 16:31:55 +0100

    uproszczenie

--- dzien07/dziedziczenie.py
+++ dzien07/dziedziczenie.py
@@ -2,16 +2,14 @@ class Czlowiek:
     def __init__(self, imie, nazwisko ):
         self._imie = imie
         self._nazwisko = nazwisko
-    def kim_jestem(self):
-        return "czlowiekiem"
     def przedstaw_sie(self):
-        print(f"Jestem {self.kim_jestem()} o imieniu {self._imie} {self._nazwisko}")
+        print(f"Jestem {self.kim_jestem} o imieniu {self._imie} {self._nazwisko}")
+    kim_jestem = "czlowiekiem"
 
 class Pracownik(Czlowiek):
     def pracuj(self):
         print(f"Pracuje... (powiedzial {self._imie})")
-    def kim_jestem(self):
-        return "pracownikiem"
+    kim_jestem = "pracownikiem"
 
 ludzie = [
     Pracownik("Zygfryd", "Z"),
