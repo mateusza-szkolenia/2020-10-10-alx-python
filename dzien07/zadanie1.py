42f0760a97b2a5a0ac7523d3ba67e2d625b86242
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-28 15:26:52 +0100

    cofanie o 1 dzien... ZLY POMYSL

--- dzien07/zadanie1.py
+++ dzien07/zadanie1.py
@@ -1,11 +1,17 @@
 import requests
+import datetime
 
 def pobierz_kurs( waluta, data ):
     url = f"http://api.nbp.pl/api/exchangerates/rates/A/{waluta}/{data}/"
     wynik = requests.get( url )
+    if wynik.ok == False:
+        wczesniej = ( datetime.date.fromisoformat(data) - datetime.timedelta(days=1) ).isoformat()
+        url = f"http://api.nbp.pl/api/exchangerates/rates/A/{waluta}/{wczesniej}/"
+        wynik = requests.get(url)
+
     return wynik
 
-data = "2019-12-26"
+data = "2019-12-25"
 waluta = "EUR"
 
 # chcialbym miec funkcje, ktora pobierze kurs obowiazujacy danego dnia:
