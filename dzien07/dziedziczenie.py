70d309572cebbb05c00eb3e892309feff103d494
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-28 16:49:11 +0100

    klasa Menedzer

--- dzien07/dziedziczenie.py
+++ dzien07/dziedziczenie.py
@@ -16,13 +16,29 @@ class Pracownik(Czlowiek):
         print(f"Pracuje... (powiedzial {self.imie_i_nazwisko})")
     kim_jestem = "pracownikiem"
 
-ludzie = [
-    Pracownik("Zygfryd", "Z"),
-    Czlowiek("Jan","X"),
-    Czlowiek("Roman", "Z")
-]
+class Menedzer(Pracownik):
+    def dodaj_pracownika(self, pracownik ):
+        self._pracownicy.append( pracownik )
+    def przedstaw_pracownikow(self):
+        for p in self._pracownicy:
+            p.przedstaw_sie()
+    kim_jestem = "menedzerem"
+    _pracownicy = []
 
-for ktos in ludzie:
-    ktos.przedstaw_sie()
-    if type(ktos) == Pracownik:
-        ktos.pracuj()
+m = Menedzer("Piotr", "S")
+p1 = Pracownik("Zygfryd", "Z")
+p2 = Pracownik("Zygmunt", "N")
+c1 = Czlowiek("Jan","X")
+c2 = Czlowiek("Roman", "Z")
+
+m.dodaj_pracownika( p1 )
+m.dodaj_pracownika( p2 )
+
+ludzie = [ m, p1, p2, c1, c2 ]
+
+#for ktos in ludzie:
+#    ktos.przedstaw_sie()
+#    if type(ktos) == Pracownik:
+#        ktos.pracuj()
+
+m.przedstaw_pracownikow()
