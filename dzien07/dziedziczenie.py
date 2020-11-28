52e2af0714276d95b3b1d53d2ff927ab32de9208
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-28 16:37:06 +0100

    dekorator @property

--- dzien07/dziedziczenie.py
+++ dzien07/dziedziczenie.py
@@ -2,13 +2,18 @@ class Czlowiek:
     def __init__(self, imie, nazwisko ):
         self._imie = imie
         self._nazwisko = nazwisko
+
+    @property
+    def imie_i_nazwisko(self):
+        return f"{self._imie} {self._nazwisko}"
+
     def przedstaw_sie(self):
-        print(f"Jestem {self.kim_jestem} o imieniu {self._imie} {self._nazwisko}")
+        print(f"Jestem {self.kim_jestem} o imieniu {self.imie_i_nazwisko}")
     kim_jestem = "czlowiekiem"
 
 class Pracownik(Czlowiek):
     def pracuj(self):
-        print(f"Pracuje... (powiedzial {self._imie})")
+        print(f"Pracuje... (powiedzial {self.imie_i_nazwisko})")
     kim_jestem = "pracownikiem"
 
 ludzie = [
