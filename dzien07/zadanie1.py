11aca831cd99c0ec9d0cbbaba81920f7b1db87ba
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-28 15:39:56 +0100

    przejscie po zleceniach i pobranie kursow walut

--- dzien07/zadanie1.py
+++ dzien07/zadanie1.py
@@ -8,7 +8,16 @@ def pobierz_kurs( waluta, data ):
     url = f"http://api.nbp.pl/api/exchangerates/rates/A/{waluta}/{startDate}/{endDate}/"
     return requests.get( url ).json()['rates'][-1]['mid']
 
+portfel = {
+    "PLN" : 0,
+    "EUR" : 0,
+    "USD" : 0
+}
+
 zlecenia = json.load( open("zlecenia.json") )
 
 for z in zlecenia:
-    print(z)
+    dz = z['data']
+    w = z['waluta']
+    kurs = pobierz_kurs(w, dz)
+    print(z, kurs)
