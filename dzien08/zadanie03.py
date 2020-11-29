22ac275ec5d374dc8fa40b3a9af21c026391c350
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-29 14:51:40 +0100

    zadanie 03

---
+++ dzien08/zadanie03.py
@@ -0,0 +1,15 @@
+# Zaimplementuj klase ElectricCar odwzorowujaca zachowanie
+# samochodu elektrycznego. Klasa powinna umozliwiac pokonanie
+# zadanego dystansu, który nie moze przekroczyc maksymalnego
+# zasiegu zdefiniowanego dla samochodu. Samochód powinien
+# miec takze mozliwosc naładowania baterii.
+# >>> car = ElectricCar(100)
+# >>> car.drive(70)
+# 70
+# >>> car.drive(50)
+# 30
+# >>> car.drive(50)
+# 0
+# >>> car.charge()
+# >>> car.drive(50)
+# 50
