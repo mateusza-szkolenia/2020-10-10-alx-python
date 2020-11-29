e0bbedddd166cef8edda01e0566d81d869e8bb1c
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-29 14:08:22 +0100

    zadanie02 - rozliczenie stawki godzinowej pracownika

---
+++ dzien08/zadanie02.py
@@ -0,0 +1,21 @@
+# Zaimplementuj klase Employee umozliwiajaca rejestrowanie czasu
+# pracy oraz wypłacanie pensji na podstawie zadanej stawki
+# godzinowej. Jezeli pracownik bedzie pracował wiecej niz 8 godzin
+# (podczas pojedynczej rejestracji czasu) to kolejne godziny policz
+# jako nadgodziny (z podwójna stawka godzinowa).
+# Przykład uzycia:
+# >>> employee = Employee('Jan', 'Nowak', 100.0)
+# >>> employee.register_time(5)
+# >>> employee.pay_salary()
+# 500.0
+# >>> employee.pay_salary()
+# 0.0
+# >>> employee.register_time(10)
+# >>> employee.pay_salary()
+# 1200.0
+# >>> employee.register_time(5)
+# >>> employee.register_time(5)
+# >>> employee.register_time(5)
+# >>> employee.register_time(5)
+# >>> employee.pay_salary()
+# 2000.0
