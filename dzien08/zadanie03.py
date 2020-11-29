43016a6dc53a7588b9285344691ba8adac3ea16e
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-29 15:10:52 +0100

    modelowanie samochodu elektrycznego

--- dzien08/zadanie03.py
+++ dzien08/zadanie03.py
@@ -13,3 +13,23 @@
 # >>> car.charge()
 # >>> car.drive(50)
 # 50
+
+class ElectricCar:
+    def __init__(self, zasieg):
+        self._zasieg_max = zasieg
+        self.charge()
+    def drive(self, dystans):
+        dystans = min( (dystans, self._zasieg) )
+        self._zasieg -= dystans
+        print(f"Przejechano {dystans}")
+    def charge(self):
+        self._zasieg = self._zasieg_max
+    _zasieg = 0
+    _zasieg_max = 0
+
+car = ElectricCar(100)
+car.drive(70)
+car.drive(50)
+car.charge()
+car.drive(80)
+car.drive(80)
