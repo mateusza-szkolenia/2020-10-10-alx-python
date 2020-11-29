892ce89d3ec9c6940447eae686938d7d2b7ab6fc
Mateusz Adamowski <m.adamowski@alx.pl>
2020-11-29 14:03:10 +0100

    zadanie 1 zrobione

--- dzien08/zadanie01.py
+++ dzien08/zadanie01.py
@@ -2,3 +2,17 @@
 # >>> product.print_info()
 # Produkt "Woda", id: 1, cena: 10.99 PLN
 
+class Produkt():
+    def __init__ (self, id, nazwa, cena):
+        self._id = id
+        self._nazwa = nazwa
+        self._cena = cena
+    def __str__(self):
+        return f'Produkt: {self._nazwa}, id: {self._id}, cena {self._cena} PLN'
+
+produkty = Produkt(1, 'Chleb', 2.99)
+produkt2 = Produkt(2, 'Mleko', 1.99)
+produkt3 = Produkt(3, 'Parówki', 4.99)
+
+for p in produkt1, produkt2, produkt3:
+    print(p)
